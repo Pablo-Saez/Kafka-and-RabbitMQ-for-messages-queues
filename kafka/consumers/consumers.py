9140c24ec49a947892a19b5ac1d0a9f5add67dec
Pablo-Saez <pablo.saez@mail.udp.cl>
2023-05-25 13:49:02 -0400

    grupos de consumidores kafka

--- kafka/consumers/consumers.py
+++ kafka/consumers/consumers.py
@@ -1,21 +1,19 @@
 from kafka import KafkaConsumer
 import random
 
-
 servidores_bootstrap = 'kafka:9092'
 topics = ['temperatura', 'porcentaje_humedad', 'posicion', 'color', 'peso']
-topic = random.choice(topics)
-print("El topic elegido es: "+ topic)
 
-consumidor = KafkaConsumer(topic, bootstrap_servers=[servidores_bootstrap])
+topic_elegido = random.choice(topics)
+grupo_consumidores = f'grupo_consumidores_{topic_elegido}'
 
-for msg in consumidor:
-    print(msg.value)
+# Configurar el consumidor con el group_id
+consumer = KafkaConsumer(
+    topic_elegido,
+    group_id=grupo_consumidores,
+    bootstrap_servers=[servidores_bootstrap]
+)
 
-
-
-
-# from kafka import KafkaConsumer
-# consumer = KafkaConsumer('mi_tema')
-# for message in consumer:
-#     print(message)
+# Consumir mensajes del topic elegido
+for msg in consumer:
+    print(f"Topic: {msg.topic}, Mensaje: {msg.value}")
